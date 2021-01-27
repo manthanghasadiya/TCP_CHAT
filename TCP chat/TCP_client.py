5ea6bba228a133eb1194f7777ab215c62b045f86
manthan <68530736+manthanghasadiya@users.noreply.github.com>
2021-01-27 09:10:42 -0500

    Add files via upload

---
+++ TCP chat/TCP_client.py
@@ -0,0 +1,19 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9997
+
+# create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host, target_port))
+
+# send some data
+client.send(b"hello")
+
+# receive some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
